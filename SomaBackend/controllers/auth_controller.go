7d9d8aef973ca49920df76e6dbf953619b0dd920
Mr-Ndi <onlythenotes@gmail.com>
2025-05-17 13:13:15 +0200

    A controller for register/login user

---
+++ SomaBackend/controllers/auth_controller.go
@@ -0,0 +1,49 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/Mr-Ndi/SomaBackend/models"
+	"github.com/Mr-Ndi/SomaBackend/services"
+	"github.com/Mr-Ndi/SomaBackend/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Register(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	hashedPass, _ := utils.HashPassword(user.Password)
+	user.Password = hashedPass
+
+	if err := services.CreateUser(&user); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not create user"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "User registered successfully"})
+}
+
+func Login(c *gin.Context) {
+	var loginData struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	if err := c.ShouldBindJSON(&loginData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := services.GetUserByEmail(loginData.Email)
+	if err != nil || !utils.CheckPasswordHash(loginData.Password, user.Password) {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	token, _ := utils.GenerateJWT(user.ID, user.Role)
+	c.JSON(http.StatusOK, gin.H{"token": token})
+}
