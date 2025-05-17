3e3cb76d24d0a311986f18275acd97c3532b734e
Mr-Ndi <onlythenotes@gmail.com>
2025-05-17 11:23:39 +0200

    Database configuration file

---
+++ SomaBackend/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, using system environment")
+	}
+}
+
+func ConnectDB() {
+	dsn := os.Getenv("DB_DSN")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to DB:", err)
+	}
+	DB = db
+	fmt.Println("Database connection established.")
+}
