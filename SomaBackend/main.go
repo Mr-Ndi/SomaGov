f0ae7fe92e0ce754b7ff208760f13ef26726dd11
Mr-Ndi <onlythenotes@gmail.com>
2025-05-17 11:19:12 +0200

    Git ignore and main file, server side logic entry point

---
+++ SomaBackend/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"SomaBackend/config"
+	"SomaBackend/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Load ENV
+	config.LoadEnv()
+
+	// Connect to DB
+	config.ConnectDB()
+
+	// Register all routes
+	routes.RegisterRoutes(r)
+
+	// Start server
+	r.Run(":8080")
+}
