3cf42db2fe9372d04b23c93b879d08320138542b
Mr-Ndi <onlythenotes@gmail.com>
2025-06-17 06:23:57 +0200

    Status to agency

--- SomaBackend/models/agency.go
+++ SomaBackend/models/agency.go
@@ -1,6 +1,8 @@
 package models
 
-import "time"
+import (
+	"time"
+)
 
 type Agency struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
@@ -8,6 +10,7 @@ type Agency struct {
 	Email     string    `json:"email"`
 	Telephone string    `json:"telephone"`
 	Address   string    `json:"address"`
+	Status    string    `json:"status"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
