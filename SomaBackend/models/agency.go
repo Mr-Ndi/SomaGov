b1c267b0994a80f0e5bd029fc7d8b9229289ecbc
Mr-Ndi <onlythenotes@gmail.com>
2025-06-17 06:19:55 +0200

    EMail to agency

--- SomaBackend/models/agency.go
+++ SomaBackend/models/agency.go
@@ -5,6 +5,7 @@ import "time"
 type Agency struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
 	Name      string    `json:"name"`
+	Email     string    `json:"email"`
 	Telephone string    `json:"telephone"`
 	Address   string    `json:"address"`
 	CreatedAt time.Time `json:"created_at"`
